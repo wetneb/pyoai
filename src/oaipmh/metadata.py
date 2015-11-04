9655ac4e0ea234e95d052334d1e368229e22c7d8
Antonin Delpeuch <antonin@delpeuch.eu>
2015-11-04 21:09:44 +0100

    Add base_dc metadata reader

--- src/oaipmh/metadata.py
+++ src/oaipmh/metadata.py
@@ -107,4 +107,41 @@ oai_dc_reader = MetadataReader(
     )
 
 
+base_dc_reader = MetadataReader(
+    fields={
+    'title':       ('textList', 'base_dc:dc/dc:title/text()'),
+    'creator':     ('textList', 'base_dc:dc/dc:creator/text()'),
+    'subject':     ('textList', 'base_dc:dc/dc:subject/text()'),
+    'description': ('textList', 'base_dc:dc/dc:description/text()'),
+    'publisher':   ('textList', 'base_dc:dc/dc:publisher/text()'),
+    'contributor': ('textList', 'base_dc:dc/dc:contributor/text()'),
+    'date':        ('textList', 'base_dc:dc/dc:date/text()'),
+    'type':        ('textList', 'base_dc:dc/dc:type/text()'),
+    'format':      ('textList', 'base_dc:dc/dc:format/text()'),
+    'identifier':  ('textList', 'base_dc:dc/dc:identifier/text()'),
+    'source':      ('textList', 'base_dc:dc/dc:source/text()'),
+    'language':    ('textList', 'base_dc:dc/dc:language/text()'),
+    'relation':    ('textList', 'base_dc:dc/dc:relation/text()'),
+    'coverage':    ('textList', 'base_dc:dc/dc:coverage/text()'),
+    'rights':      ('textList', 'base_dc:dc/dc:rights/text()'),
+    'autoclasscode':('textList', 'base_dc:dc/base_dc:autoclasscode/text()'),
+    'classcode':('textList', 'base_dc:dc/base_dc:classcode/text()'),
+    'collection':('textList', 'base_dc:dc/base_dc:collection/text()'),
+    'collname':('textList', 'base_dc:dc/base_dc:collname/text()'),
+    'continent':('textList', 'base_dc:dc/base_dc:continent/text()'),
+    'country':('textList', 'base_dc:dc/base_dc:country/text()'),
+    'lang':('textList', 'base_dc:dc/base_dc:lang/text()'),
+    'link':('textList', 'base_dc:dc/base_dc:link/text()'),
+    'oa':('textList', 'base_dc:dc/base_dc:oa/text()'),
+    'rightsnorm':('textList', 'base_dc:dc/base_dc:rightsnorm/text()'),
+    'typenorm':('textList', 'base_dc:dc/base_dc:typenorm/text()'),
+    'year':('textList', 'base_dc:dc/base_dc:year/text()'),
+    },
+    namespaces={
+    'base_dc': 'http://oai.base-search.net/base_dc/',
+    'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+    'dc' : 'http://purl.org/dc/elements/1.1/'}
+    )
+
+
     
